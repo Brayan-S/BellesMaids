2eeed16943e8932fe7b64b2f5dc46cbc565fc9de
Brayan Solis <brayan.develope777@gmail.com>
2021-05-06 16:24:16 -0500

    ..

--- contactfrom.php
+++ contactfrom.php
@@ -4,11 +4,11 @@ if (isset($_POST['submit'])){
     $number = $_POST['number'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
- 
+
    $mailTo = "estebanm289@outlook.com";
    $headers = "from: ".$mailFrom;
    $txt = "you have recieved an e-mail from".$name.".\n\n".$message;
- 
+
     mail($mailTo, $number, $txt, $headers );
     header("location: index.php?mailsend");
 }
