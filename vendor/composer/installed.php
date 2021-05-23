9c71e0571ab1e2bf841c646b044f5aadd7b3b836
Brayan Solis <brayan.develope777@gmail.com>
2021-05-23 17:35:37 -0500

    lock.file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,49 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b9e3bbab15fd54454384fe67d5c49816b1352e71',
+    'name' => 'bellesmaids/composer',
+  ),
+  'versions' => 
+  array (
+    'bellesmaids/composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9e3bbab15fd54454384fe67d5c49816b1352e71',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c861fcba2ca29404dc9e617eedd9eff4616986b8',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+  ),
+);
